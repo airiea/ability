aff20dc94d7c889f3e199af0745a024d9ce4d13f
yifan lu <luyifane@amazon.com>
2023-10-25 22:50:57 -0400

    init ability package

---
+++ dao/src/main/java/com/airiea/ability/dao/AbilityDao.java
@@ -0,0 +1,8 @@
+package com.airiea.ability.dao;
+
+/**
+ * Data Access Object (DAO) interface for tasks.
+ * This provides methods to perform operations on tasks.
+ */
+public interface AbilityDao {
+}
